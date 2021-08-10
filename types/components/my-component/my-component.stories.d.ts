96f5853d851bf3bfe8e254937dbae92a5995dfa9
TheodoreGC <TheodoreGC@users.noreply.github.com>
2021-08-10 11:02:42 +0000

    Deploying to gh-pages from  @ b11368420942bbf6f2af9cd9a7e3d98e14132244 🚀

---
+++ types/components/my-component/my-component.stories.d.ts
@@ -0,0 +1,10 @@
+declare const _default: {
+  title: string;
+  decorators: ((...args: any[]) => any)[];
+  parameters: {
+    notes: string;
+    jest: string[];
+  };
+};
+export default _default;
+export declare const Introduction: any;
